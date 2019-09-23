fc7af7f5b6db452c174df3a39457a389197a611b
Ignacio Morales <imorales@tiempodevelopment.com>
2019-09-22 22:00:46 -0500

    Adding Contract Service and Unit Testing

---
+++ GeoLib.Test/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using GeoLib.Contracts;
+using GeoLib.Data;
+using GeoLib.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GeoLib.Test
+{
+    [TestClass]
+    public class ManagerTest
+    {
+        [TestMethod]
+        public void test_zip_code_retrieval()
+        {
+            Mock<IZipCodeRepository> mockZipCodeRepository = new Mock<IZipCodeRepository>();
+
+            ZipCode zipCode = new ZipCode()
+            {
+                City = "LINCOLN PARK",
+                State = new State() { Abbreviation = "NJ" },
+                Zip = "07035"
+            };
+
+            mockZipCodeRepository.Setup(obj => obj.GetByZip("07035")).Returns(zipCode);
+
+            IGeoService geoService = new GeoManager(mockZipCodeRepository.Object);
+            ZipCodeData data = geoService.GetZipInfo("07035");
+
+            Assert.IsTrue(data.City.ToUpper() == "LINCOLN PARK");
+            Assert.IsTrue(data.State == "NJ");
+        }
+    }
+}
