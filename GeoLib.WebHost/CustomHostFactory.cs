799506d486e5828f7a24ab873385d02b23094f3c
Ignacio Morales <imorales@tiempodevelopment.com>
2019-09-23 11:24:26 -0500

    Adding Host Applications

---
+++ GeoLib.WebHost/CustomHostFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Web;
+
+namespace GeoLib.WebHost
+{
+    public class CustomHostFactory : ServiceHostFactory
+    {
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            //return base.CreateServiceHost(serviceType, baseAddresses);
+
+            //Code only for visualization, it is what previous code call already does
+            ServiceHost host = new ServiceHost(serviceType, baseAddresses);
+            return host;
+        }
+    }
+}
